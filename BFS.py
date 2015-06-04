5cbc17de4a043d6cb8175c6b6fa4d2138ea40ab7
Deepit Patel <detel@users.noreply.github.com>
2015-06-04 23:57:29 +0530

    Breadth First Search
    Breadth First Search implementation in Python

---
+++ BFS.py
@@ -0,0 +1,16 @@
+def BFS(s,graph):
+    level = {s:0}
+    parent = {s:None}
+    i = 1
+    frontier = [s]
+    while frontier:
+        next = []
+        for u in frontier:
+            for v in graph[u]:
+                if v not in level:
+                    level[v] = i
+                    parent[v] = u
+                    next.append(v)
+                vertex = u
+        frontier = next
+        i += 1
