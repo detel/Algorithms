c62f4aa9dfa7f448df15425447e36fbba3175267
Deepit Patel <detel@users.noreply.github.com>
2015-07-07 10:31:14 +0530

    0/1 KnapSack

--- Knapsack.py
+++ Knapsack.py
@@ -3,8 +3,8 @@ def knapSack(S,size,values):
 	n = len(values)
 	K = [[0 for i in range(S+1)]for j in range(n+1)]
 
-	for i in xrange(n+1):
-		for j in xrange(S+1):
+	for i in xrange(1,n+1):
+		for j in xrange(1,S+1):
 			if size[i-1] <= j:
 				K[i][j] = max(values[i-1]+K[i-1][j-size[i-1]],  K[i-1][j])
 			else:
