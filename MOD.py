9a841cee7cc176d6343db554c531134d66a6768e
Deepit Patel <detel@users.noreply.github.com>
2015-07-22 17:01:23 +0530

    Modular Exponentiation in Python
    By the way, it's in-built in Python :D

---
+++ MOD.py
@@ -0,0 +1,9 @@
+#Calculate (x ** y) % z efficiently.
+def pow_mod(x, y, z):
+    number = 1
+    while y:
+        if y & 1:
+            number = number * x % z
+        y >>= 1
+        x = x * x % z
+    return number
