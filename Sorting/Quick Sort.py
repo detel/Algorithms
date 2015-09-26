369e9e425912a09a0acfbfabefa939c82e5336a7
Deepit Patel <detel@users.noreply.github.com>
2015-09-26 09:01:25 +0530

    Quick Sort

--- Sorting/Quick Sort.py
+++ Sorting/Quick Sort.py
@@ -1,3 +1,5 @@
+import random
+
 def quickSort1(ar):
     if len(ar) < 2:
         return(ar)
@@ -20,7 +22,25 @@ def quickSort2(arr):
     greater = quickSort([x for x in arr if x > arr[0]])
 
     return lesser + pivots + greater
-
+    
+def quickSort( array, low, high ):
+    if low < high:
+      pivot = partition( array, low, high )
+      quickSort( array, low, pivot - 1 )
+      quickSort( array, pivot + 1, high )
+ 
+ 
+def partition( array, low, high ) :
+    pivot = low + random.randrange( high - low + 1 )
+    array[pivot], array[high] = array[high], array[pivot]
+    
+    for i in range( low, high ):
+      if array[i] <= array[high]:
+        array[i], array[low] = array[low], array[i]
+        low += 1
+ 
+    array[low], array[high] = array[high], array[low]
+    return low
 
 ar = map(int, raw_input().split())
-print quickSort1(ar),quickSort2(ar) 
+print quickSort(ar, 0, len(ar)-1), quickSort1(ar), quickSort2(ar) 
