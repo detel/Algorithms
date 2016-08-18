bc5b2a3c3b6993523b49d9bc3bb0155908a847ce
Deepit Patel <detel@users.noreply.github.com>
2016-08-18 16:32:21 +0530

    Centroid Decomposition Of A Tree

--- Graph-Theory/Centroid Decomposition.java
+++ Graph-Theory/Centroid Decomposition.java
@@ -5,13 +5,13 @@
 import java.util.*;
 import java.io.*;
 
-public class CentroidDecomposition {
+public class XeniaAndTree {
     public static InputReader in;
     public static PrintWriter out;
 
     public static final int MOD = (int) (1e9 + 7);
     public static ArrayList<Integer>[] graph;
-    public static int[] P, subSize, label, dist[];
+    public static int[] centroidTreeParent, centroidTreeLevel, subTreeSize, dist[];
     public static int LOGMAXN;
     
     @SuppressWarnings("unchecked")
@@ -20,41 +20,41 @@ public class CentroidDecomposition {
         out = new PrintWriter(System.out);
         
         int n = in.nextInt(),
-            m = in.nextInt();
+              m = in.nextInt();
         graph = new ArrayList[n];
         for(int i = 0; i < n; ++i)  graph[i] = new ArrayList<Integer>();
         for(int i = 1; i < n; ++i) {
             int u = in.nextInt() - 1,
-                v = in.nextInt() - 1;
+                  v = in.nextInt() - 1;
             graph[u].add(v);
             graph[v].add(u);
         }
         
         // construct centroid tree in P
-        P = new int[n];
-        subSize = new int[n];
-        label = new int[n];
-        Arrays.fill(P, -2);
+        centroidTreeParent = new int[n];
+        subTreeSize = new int[n];
+        centroidTreeLevel = new int[n];
+        Arrays.fill(centroidTreeParent, -2);
         decompose(0, -1, n, 0);
 
         // preprocess needed distances
         LOGMAXN = (int)(Math.log(n)/Math.log(2) + 3);
         dist = new int[LOGMAXN][n];
         for(int i = 0; i < n; ++i)
-            computeDist(i, label[i], i, -1, 0);
+            computeDist(i, centroidTreeLevel[i], i, -1, 0);
         
         // process queries
         near = new int[n];
         Arrays.fill(near, n);
-        StringBuilder sb = new StringBuilder();
+        StringBuilder result = new StringBuilder();
         update(0);
         while(m-->0)
             if(in.nextInt() == 1)
                 update(in.nextInt() - 1);
             else
-                sb.append(query(in.nextInt() - 1)+"\n");
+                result.append(query(in.nextInt() - 1)+"\n");
         
-        out.print(sb);
+        out.print(result);
 
         out.close();
     }
@@ -65,21 +65,21 @@ public class CentroidDecomposition {
     public static int[] near;
     public static void update(int v) {
         int u = v,
-            lb = label[v];
+              lb = centroidTreeLevel[v];
         while(u != -1) {
             near[u] = Math.min(near[u], dist[lb][v]);
-            u = P[u];
+            u = centroidTreeParent[u];
             --lb;
         }
     }
     
     public static int query(int v) {
         int u = v,
-            lb = label[v],
-            ans = Integer.MAX_VALUE;
+              lb = centroidTreeLevel[v],
+              ans = Integer.MAX_VALUE;
         while(u != -1) {
             ans = Math.min(ans, near[u] + dist[lb][v]);
-            u = P[u];
+            u = centroidTreeParent[u];
             --lb;
         }
         return ans;
@@ -89,10 +89,10 @@ public class CentroidDecomposition {
         int sz = 1;
         for(int i = 0, size = graph[u].size(); i < size; ++i) {
             int v = graph[u].get(i);
-            if(v != p && P[v] == -2)
+            if(v != p && centroidTreeParent[v] == -2)
                 sz += findSize(v, u);
         }
-        return subSize[u] = sz;
+        return subTreeSize[u] = sz;
     }
 
     public static int decompose(int u, int p, int n, int lb) {
@@ -101,7 +101,7 @@ public class CentroidDecomposition {
         int idx = -1;
         for(int i = 0, size = graph[u].size(); i < size; ++i) {
             int v = graph[u].get(i);
-            if(v != p && P[v] == -2 && subSize[v] > n / 2) {
+            if(v != p && centroidTreeParent[v] == -2 && subTreeSize[v] > n / 2) {
                 idx = v;
                 break;
             }
@@ -110,13 +110,13 @@ public class CentroidDecomposition {
             return decompose(idx, u, n, lb);
 
         //u is centroid, decompose forest
-        P[u] = -1;
+        centroidTreeParent[u] = -1;
         for(int i = 0, size = graph[u].size(); i < size; ++i) {
             int v = graph[u].get(i);
-            if(P[v] == -2)
-                P[decompose(v, -1, n, lb + 1)] = u;
+            if(centroidTreeParent[v] == -2)
+                centroidTreeParent[decompose(v, -1, n, lb + 1)] = u;
         }
-        label[u] = lb;
+        centroidTreeLevel[u] = lb;
         return u;
     }
     
@@ -127,7 +127,7 @@ public class CentroidDecomposition {
         dist[lb][u] = d;
         for(int i = 0, size = graph[u].size(); i < size; ++i) {
             int v = graph[u].get(i);
-            if(v != p && label[v] >= lb)
+            if(v != p && centroidTreeLevel[v] >= lb)
                 computeDist(s, lb, v, u, d + 1);
         }
     }
